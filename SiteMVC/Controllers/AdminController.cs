f69811b5334da049f66b8b35fa4d14746ef47d2c
DENIELER <danielostapenko@gmail.com>
2013-07-17 10:13:36 +0300

    [SiteMVC] Start admin part

---
+++ SiteMVC/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult AddSubPurchases()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SiteMVC/ViewModels/SubPurchase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteMVC.ViewModels
+{
+    public class SubPurchase
+    {
+
+    }
+}
