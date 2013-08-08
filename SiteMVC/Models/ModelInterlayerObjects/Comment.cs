191b6e25fd8b8c4b8e110caaa06f3b284932c2d0
DENIELER <danielostapenko@gmail.com>
2013-08-08 10:34:36 +0300

    [SiteMVC] Add Comments entities and markup for comments in adv list

---
+++ SiteMVC/Models/ModelInterlayerObjects/Comment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteMVC.Models.ModelInterlayerObjects
+{
+    public class Comment
+    {
+        public Comment(AdvertismentComment comment)
+        {
+            this.CommentID = comment.CommentID;
+            this.Message = comment.Message;
+            this.CreateDate = comment.CreateDate;
+            this.AdvertismentID = comment.AdvertismentID;
+
+            this.Login = comment.Login;
+            this.User = new User(comment.User);
+        }
+
+        private string Login { get; set; }
+
+        public Guid CommentID { get; set; }
+        public string Message { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int AdvertismentID { get; set; }
+        public User User { get; set; }
+
+        public string DisplayLogin
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Login))
+                    return Login;
+                else if (User != null)
+                    return User.DisplayLogin;
+                else return "Аноним";
+            }
+        }
+    }
+}
---
+++ SiteMVC/Models/ModelInterlayerObjects/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteMVC.Models.ModelInterlayerObjects
+{
+    public class User
+    {
+        public User(Models.User user)
+        {
+            this.UserID = user.UserID;
+            this.Login = user.Login;
+            this.Email = user.Email;
+        }
+
+        private string Login { get; set; }
+        private string Email { get; set; }
+
+        public int UserID { get; set; }
+        
+        public string DisplayLogin 
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(this.Login))
+                    return Login;
+                else
+                {
+                    int emailSeparatorIndex = this.Email.IndexOf("@");
+                    if (emailSeparatorIndex > 0)
+                        return this.Email.Substring(0, emailSeparatorIndex);
+                    else return "Аноним";
+                }
+            }
+        }
+    }
+}
